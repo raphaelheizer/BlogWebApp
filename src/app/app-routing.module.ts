a7899305e8a0e10100f5f260c7d98e2d12fbc405
= <raphaelheizer@gmail.com>
2021-11-21 02:02:16 -0300

    Added basic project structure, Added authentication components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundComponent} from './view/not-found/not-found.component';
+import {LoginComponent} from './view/authentication/login.component';
+import {RegisterComponent} from './view/register/register.component';
+import {BlogComponent} from './view/blog/blog.component';
+import {PasswordRecoverComponent} from './view/password-recover/password-recover.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', pathMatch: 'full', component: BlogComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'password-recover', component: PasswordRecoverComponent},
+    {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './view/blog/blog.component';
+import { NotFoundComponent } from './view/not-found/not-found.component';
+import { LoginComponent } from './view/authentication/login.component';
+import { RegisterComponent } from './view/register/register.component';
+import { PasswordRecoverComponent } from './view/password-recover/password-recover.component';
+import { PostComponent } from './view/post/post.component';
+import { PostEditorComponent } from './view/post-editor/post-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    NotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    PasswordRecoverComponent,
+    PostComponent,
+    PostEditorComponent
   ],
   imports: [
     BrowserModule,
